c90eddd152d1d0042d7fa1fc56bc308f60d9485c
Laolu Ade <laoluadewoye@gmail.com>
2025-09-17 20:51:34 -0400

    init commit

---
+++ src/app/home/home.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { Greeting } from '../components/greeting/greeting';
+
+@Component({
+  selector: 'app-home',
+  imports: [Greeting],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+}
